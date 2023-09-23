054399047780abe8a1267b54729a1967f6a5e78b
Natalia Balashova <88karamelka88@mail.ru>
2023-09-23 22:49:10 +0300

    create table

---
+++ practice-2/assets/js/index.js
@@ -0,0 +1,21 @@
+const table = document.querySelector('#table');
+
+function createTable(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach((element) => {
+            console.log(element)
+            let row = document.createElement('tr');
+            row.className = 'tableRow';
+            row.innerHTML += `<tbody>
+            <td>${element.id}</td>
+            <td>${element.userId}</td>
+            <td>${element.title}</td>
+            <td>${element.body}</td></tbody>`
+            table.append(row);
+        });
+    });
+};
+
+createTable();
