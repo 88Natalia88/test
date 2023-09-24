8b15870e2514c272efa05ddafd2444f4b3fffda2
Natalia Balashova <88karamelka88@mail.ru>
2023-09-25 00:18:06 +0300

    create sorting

--- practice-3/assets/js/index.js
+++ practice-3/assets/js/index.js
@@ -1,5 +1,5 @@
-const table = document.querySelector('#table');
-
+const table = document.querySelector('#table tbody');
+//создание таблицы с данными
 function createTable(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
@@ -18,13 +18,13 @@ function createTable(){
 };
 
 createTable();
-
+//поисковая строка
 const search = document.querySelector('#search');
 const searchBtn = document.querySelector('#search-btn');
 
 
 searchBtn.addEventListener("click", searchText);
-
+//поиск по таблице
 function searchText(e){
     e.preventDefault();
     let searchAdd = search.value;
@@ -48,4 +48,42 @@ function searchText(e){
         });
     }
     search.value = '';
+}
+//сортировка
+const tableHeaders = document.querySelectorAll('th');
+
+tableHeaders.forEach((header) => {
+    //console.log(header)
+    header.addEventListener("click", () => {
+        //console.log(header)
+        sortTable(header.cellIndex, header.dataset.type, header)
+    });
+    header.isSort = false;
+})
+
+function sortTable(colNum, type, header){
+    let tbody = document.querySelector('tbody');
+    //console.log(tbody)
+    let rowsArray = Array.from(tbody.rows);
+    //console.log(rowsArray)
+    let compare;
+    switch(type){
+        case 'number':
+            compare = function(rowA, rowB){
+                return header.isSort
+                    ? rowA.cells[colNum].innerHTML - rowB.cells[colNum].innerHTML
+                    : rowB.cells[colNum].innerHTML - rowA.cells[colNum].innerHTML;
+            }
+            break;
+            case 'string':
+                compare = function(rowA, rowB){
+                    return header.isSort
+                    ? (rowA.cells[colNum].innerHTML > rowB.cells[colNum].innerHTML ? 1 : -1)
+                    : (rowA.cells[colNum].innerHTML < rowB.cells[colNum].innerHTML ? 1 : -1);
+                }
+                break;
+    }
+    rowsArray.sort(compare);
+    tbody.append(...rowsArray);
+    header.isSort = !header.isSort;
 }
