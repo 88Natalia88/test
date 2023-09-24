02179b3caa57902fec69616e48e6bc9e196c876b
Natalia Balashova <88karamelka88@mail.ru>
2023-09-24 19:37:23 +0300

    create function search

--- practice-3/assets/js/index.js
+++ practice-3/assets/js/index.js
@@ -5,17 +5,47 @@ function createTable(){
     .then(response => response.json())
     .then(data => {
         data.forEach((element) => {
-            console.log(element)
             let row = document.createElement('tr');
             row.className = 'tableRow';
-            row.innerHTML += `<tbody>
+            row.innerHTML += `
             <td>${element.id}</td>
             <td>${element.userId}</td>
             <td>${element.title}</td>
-            <td>${element.body}</td></tbody>`
+            <td>${element.body}</td>`
             table.append(row);
         });
     });
 };
 
 createTable();
+
+const search = document.querySelector('#search');
+const searchBtn = document.querySelector('#search-btn');
+
+
+searchBtn.addEventListener("click", searchText);
+
+function searchText(e){
+    e.preventDefault();
+    let searchAdd = search.value;
+    
+    const regSearch = searchAdd.toLowerCase();
+    if(searchAdd.length >= 3){
+        const tableRows = document.querySelectorAll('#table .tableRow');
+        tableRows.forEach((row) => {
+            const rowData = row.textContent;
+            if(rowData.toLowerCase().includes(regSearch)){
+                row.style.display = 'table-row';
+            } else {
+                row.style.display = 'none';
+            }
+        });
+    } else {
+        const tableRows = document.querySelectorAll("#table .tableRow");
+    
+        tableRows.forEach((row) => {
+            row.style.display = "table-row";
+        });
+    }
+    search.value = '';
+}
