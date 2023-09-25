99a81ef34df202d7b462cb980cb60c83b464ccd4
Natalia Balashova <88karamelka88@mail.ru>
2023-09-25 16:41:31 +0300

    change function submitForm

--- practice-1/assets/js/index.js
+++ practice-1/assets/js/index.js
@@ -1,24 +1,3 @@
-//кастомную кнопку "выберите файл" связываем с input type="file"
-document.querySelector(".form__img-castomBtn").addEventListener("click", function(){
-    document.querySelector("input[type='file']").click();
-});
-//откраваем модальное окно
-const modal = document.querySelector(".modal");
-const button = document.querySelector(".button");
-const btnMain = document.getElementById("btn-main");
-
-
-btnMain.addEventListener("click", function(){
-    modal.style.display = 'block';
-    button.style.display = "none";
-});
-//закрываем модальное окно
-const btnNo = document.querySelector('.button-no');
-btnNo.addEventListener("click", function(){
-    modal.style.display = "none";
-    button.style.display = "block";
-});
-//валидация
 //переменные всех инпутов
 let errors = document.querySelectorAll(".form__error");
 const text = document.getElementById('text');
@@ -35,10 +14,53 @@ const boss= document.getElementById('boss');
 const selectForm = document.getElementById('select');
 const btnYes = document.getElementById("button-yes");
 
+//кастомную кнопку "выберите файл" связываем с input type="file"
+document.querySelector(".form__img-castomBtn").addEventListener("click", function(){
+    document.querySelector("input[type='file']").click();
+});
+
+//откраваем модальное окно
+const modal = document.querySelector(".modal");
+const button = document.querySelector(".button");
+const btnMain = document.getElementById("btn-main");
+
+
+btnMain.addEventListener("click", function(){
+    modal.style.display = 'block';
+    button.style.display = "none";
+});
+//закрываем модальное окно
+const btnNo = document.querySelector('.button-no');
+btnNo.addEventListener("click", function(){
+    modal.style.display = "none";
+    button.style.display = "block";
+
+    // Очистка полей формы
+    text.value = '';
+    tel.value = '';
+    email.value = '';
+    img.value = '';
+    ru.value = '';
+    vk.value = '';
+    ok.value = '';
+    instagram.value = '';
+    facebook.value = '';
+    youtube.value = '';
+    boss.value = '';
+    selectForm.value = '';
+
+    // Очистка сообщений об ошибках
+    errors.forEach((error) => {
+        error.innerHTML = '';
+    });
+});
+//валидация
+
 btnYes.addEventListener("click", submitForm);
 
 
-function submitForm(){
+function submitForm(e){
+    e.preventDefault();
     //данные введенные в инпутах
     let textAdd = text.value;
     let telAdd = tel.value;
@@ -59,20 +81,25 @@ errors.forEach((error, i) => {
 
     if(textAdd === '' && i === 0){
         error.innerHTML = "* Введите наименование организации";
+        formValid = false;
     }
 
     let regTel = /^\+?\d{1,}\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{2}[\s.-]?\d{2}$/; //регулярное выражение для корректного номера телефона
     if(telAdd === '' && i === 1){
         error.innerHTML = "* Введите номер телефона";
+        formValid = false;
     } else if (telAdd !== '' && i === 1 && !regTel.test(telAdd)) {
         error.innerHTML = "* Введите корректный номер телефона";
+        formValid = false;
     }
 
     let regEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //регулярное выражение для проверки корректного email
     if(emailAdd === '' && i === 2){
         error.innerHTML = "* Введите ваш E-mail";
+        formValid = false;
     } else if (emailAdd !== '' && i === 2 && !regEmail.test(emailAdd)){
         error.innerHTML = "* Введите корректный e-mail";
+        formValid = false;
     }
 
     let regPhoto = /\.(jpeg|jpg|png)$/i; //регулярное выражение для проверки формата добавленного фото
@@ -84,55 +111,17 @@ errors.forEach((error, i) => {
 
     if(selectFormAdd === '' && i === 4){
         error.innerHTML = "* Выберите направление";
+        formValid = false;
     }
 
-    let regUrl = /^(http|https):\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //регулярное выражение для корректного ввода сайта
-    if(ruAdd === '' && i === 5){
-        error.innerHTML = "* Введите ссылку";
-    } else if (ruAdd !== '' && i === 5 && !regUrl.test(ruAdd)){
-        error.innerHTML = "* Введите корректную ссылку";
-    }
-
-    let regSocial = /(vk\.com|ok\.ru|facebook\.com|instagram\.com|youtube\.com)/i; //регулярное выражение для корректного ввода социальных сетей
-    if(vkAdd === '' && i === 6){
-        error.innerHTML = "* Введите ссылку";
-    } else if (vkAdd !== '' && i === 6 && !regSocial.test(vkAdd)){
-        error.innerHTML = "* Введите корректную ссылку";
-    }
-
-    if(okAdd === '' && i === 7){
-        error.innerHTML = "* Введите ссылку";
-    } else if (okAdd !== '' && i === 7 && !regSocial.test(okAdd)){
-        error.innerHTML = "* Введите корректную ссылку";
-    }
-
-    if(instagramAdd === '' && i === 8){
-        error.innerHTML = "* Введите ссылку";
-    } else if (instagramAdd !== '' && i === 8 && !regSocial.test(instagramAdd)){
-        error.innerHTML = "* Введите корректную ссылку";
-    }
-
-    if(facebookAdd === '' && i === 9){
-        error.innerHTML = "* Введите ссылку";
-    } else if (facebookAdd !== '' && i === 9 && !regSocial.test(facebookAdd)){
-        error.innerHTML = "* Введите корректную ссылку";
-    }
-
-    if(youtubeAdd === '' && i === 10){
-        error.innerHTML = "* Введите ссылку";
-    } else if (youtubeAdd !== '' && i === 10 && !regSocial.test(youtubeAdd)){
-        error.innerHTML = "* Введите корректную ссылку";
-    }
-
-    if(bossAdd === '' && i === 11){
-        error.innerHTML = "* Введите ФИО руководителя";
-    }
-
-    if (error.innerHTML !== '') {
+    if(ruAdd === '' || vkAdd === '' || okAdd === '' || instagramAdd === '' || facebookAdd === '' || youtubeAdd === '' || bossAdd === ''){
         formValid = false;
     }
 
 })
+if(!formValid){
+    console.log("Заполните все поля");
+}
 
 if (formValid) {
     // Очистить все инпуты и отправка данных формы
